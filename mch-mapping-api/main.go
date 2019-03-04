942c1768ea2444d3c4992c0127e9dc2f418477a0
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2019-03-04 09:18:19 +0100

    Removing unrelated code and adding API usage route

--- mch-mapping-api/main.go
+++ mch-mapping-api/main.go
@@ -4,66 +4,37 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
 
 	"github.com/aphecetche/pigiron/mapping"
-	"github.com/aphecetche/pigiron/mch-mapping-api/output"
-	"github.com/aphecetche/pigiron/segcontour"
 	"github.com/spf13/viper"
 
 	// must include the specific implementation package of the mapping
 	_ "github.com/aphecetche/pigiron/mapping/impl4"
 )
 
-func showSegmentation() http.HandlerFunc {
-	// TODO : initialize a global cseg cache here (beware mutex)
-	// to be used in the lambda below
+func usage() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		q := r.URL.Query()
-		deid, err := strconv.Atoi(q.Get("deid"))
-		if err != nil {
-			w.Write([]byte("Malformed deid"))
-			return
-		}
-		bending, err := strconv.ParseBool(q.Get("bending"))
-		if err != nil {
-			w.Write([]byte("Malformed bending"))
-			return
-		}
-		showDEs, err := strconv.ParseBool(q.Get("des"))
-		if err != nil {
-			showDEs = false
-		}
-		showPads, err := strconv.ParseBool(q.Get("pads"))
-		if err != nil {
-			showPads = false
-		}
-		showDualSampas, err := strconv.ParseBool(q.Get("dualsampas"))
-		if err != nil {
-			showDualSampas = false
-		}
-		cseg := mapping.NewCathodeSegmentation(mapping.DEID(deid), bending)
+		fmt.Fprintf(w, `<h1>MCH Mapping API</h1>
 
-		formats, ok := q["format"]
-		format := "svg"
-		if ok {
-			format = formats[0]
-		}
+<p>This API gives 2D geometric information about detection elements, dual sampas and pads.</p>
 
-		showflags := segcontour.ShowFlags{DEs: showDEs, DualSampas: showDualSampas, Pads: showPads, PadChannels: true}
+<h2>Detection element plane envelop</h2>
 
-		switch strings.ToLower(format) {
-		case "svg":
-			output.ToSVG(w, cseg, bending, showflags)
-		case "d3":
-			output.ToD3(w, cseg, bending, showflags)
-		default:
-			fmt.Fprintf(w, "Don't know how to deal with format %s\n", format)
-		}
+<pre>/degeo?deid=[number]&bending=[true|false]</pre>
+
+<p>both deid and bending options are required</p>
+
+<h2>Dual sampa envelops</h2>
+
+<pre>/dualsampas?deid=[number]&bending=[true|false]</pre>
+
+<p>Returns the vertices of the polygons describing the outline of all the dual sampas 
+of a given detection element plane</p>
+`)
 	}
 }
 
-func DualSampas() http.HandlerFunc {
+func dualSampas() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		q := r.URL.Query()
@@ -78,11 +49,11 @@ func DualSampas() http.HandlerFunc {
 			return
 		}
 		cseg := mapping.NewCathodeSegmentation(mapping.DEID(deid), bending)
-		output.JSONDualSampas(w, cseg, bending)
+		jsonDualSampas(w, cseg, bending)
 	}
 }
 
-func DualSampaPads() http.HandlerFunc {
+func dualSampaPads() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		q := r.URL.Query()
 		deid, err := strconv.Atoi(q.Get("deid"))
@@ -96,11 +67,11 @@ func DualSampaPads() http.HandlerFunc {
 			return
 		}
 		seg := mapping.NewSegmentation(mapping.DEID(deid))
-		output.JSONDualSampaPads(w, seg, dsid)
+		jsonDualSampaPads(w, seg, dsid)
 	}
 }
 
-func DEGeo() http.HandlerFunc {
+func deGeo() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		q := r.URL.Query()
@@ -115,24 +86,21 @@ func DEGeo() http.HandlerFunc {
 			return
 		}
 		cseg := mapping.NewCathodeSegmentation(mapping.DEID(deid), bending)
-		output.JSONDEGeo(w, cseg, bending)
+		jsonDEGeo(w, cseg, bending)
 	}
 }
 
 func main() {
-
 	viper.SetEnvPrefix("MCH")
 	viper.BindEnv("MAPPING_API_PORT")
 	viper.SetDefault("MAPPING_API_PORT", 8080)
-	fs := http.FileServer(http.Dir("assets"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	http.HandleFunc("/", showSegmentation())
-	http.HandleFunc("/dualsampas", DualSampas())
-	http.HandleFunc("/dualsampapads", DualSampaPads())
-	http.HandleFunc("/degeo", DEGeo())
 	port := viper.GetInt("MAPPING_API_PORT")
 	fmt.Println("Started server to listen on port", port)
+
+	http.HandleFunc("/", usage())
+	http.HandleFunc("/dualsampas", dualSampas())
+	http.HandleFunc("/dualsampapads", dualSampaPads())
+	http.HandleFunc("/degeo", deGeo())
 	if err := http.ListenAndServe(":"+strconv.Itoa(port), nil); err != nil {
 		panic(err)
 	}
